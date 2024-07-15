6a1f04812ebc0a1b3c1aadb1971faf35af8c2a0b
Tom O'Leary <tr.oleary@outlook.com>
2024-07-15 09:04:12 -0400

    add item validation and sorting

--- consolidation.py
+++ consolidation.py
@@ -7,7 +7,7 @@ Logic:
     Limits: total_volume < 70.0 & total_weight < 1500
     Conditions:
         1. All items must be within 7 days of earliest ready_date in container
-        2. Items in container should ideally have the same 'supplier', but not required
+        2. Containers must not exceed volume & weight limits
 
 Response:
     1. Generates csv if as_csv is True
@@ -23,19 +23,36 @@ Example Item:
         "ready_date": "2021-01-01",
     }
 """
+import pandas as pd
 from collections import defaultdict
-from typing import Union
 
-from container import Container, ConsolidationError
+from container import Container, ConsolidationError, Item
+
+
+class GeneratorError(Exception):
+    pass
 
 
 class ConsolidationGenerator:
     containers: list
     items: defaultdict
+    sort_key: str
+    sort_reverse: bool
 
     NULL_VALUES = ("", "nan", "Nan", "NaN", None, "null")
 
-    def __init__(self, as_csv: bool = False):
+    VALID_SORT_KEYS = [
+        "ready_date",
+        "supplier",
+        "total_volume",
+        "total_weight",
+    ]
+
+    CSV_PATH = ""
+    CSV_OUTPUT_PATH = "container_output.csv"
+
+    def __init__(self, sort_key: str = None, as_csv: bool = False):
+        self._determine_sorting(sort_key or "ready_date")  # ready_date ascending is default
         self.as_csv = as_csv
 
         self.containers = []
@@ -46,33 +63,38 @@ class ConsolidationGenerator:
         self._query_data()
         self._group_results()
 
+        for locations, items in self.items.items():
+            if not items:
+                continue
+
+            self._consolidate(self._sorted_items(items), locations)
+
+        if self.as_csv:
+            self._generate_csv_output()
+
         return self.containers
 
     def _query_data(self):
-        self.query_results = []
+        """Data being retrieved from csv example file, ideally would be queried from a db"""
+        self.query_results = pd.read_csv(self.CSV_PATH).to_dict("records")
 
     def _group_results(self):
         for row in self.query_results:
-            if not row.get("ready_date"):
+            try:
+                row_item = Item(**row)
+            except TypeError as e:
+                # missing data of some sort
+                # future state should log these for tracking instead of printing
+                print(e)
                 continue
 
-            pickup = row.get("pick_up_location")
-            deliver_to = row.get("deliver_to_location")
-
-            key = (pickup, deliver_to)
-            if any(isinstance(k, float) for k in key):
-                # NaN values
-                continue
-            elif any(k in self.NULL_VALUES for k in key):
-                continue
-
-            self.items[key].append(row)
+            self.items[row_item.key].append(row_item)
 
     def _consolidate(self, items: list, locations: tuple):
         containers = [Container(*locations, item=items.pop(0))]
         for item in items:
             new_container = Container(*locations, item=item)
-            containers = self._sorted_containers(containers, new_container)
+            # containers = self._sorted_items(containers, new_container)
             for container in containers:
                 try:
                     container += new_container
@@ -85,6 +107,19 @@ class ConsolidationGenerator:
         
         self.containers.extend(containers)
 
-    @staticmethod
-    def _sorted_containers(current_containers: list, new_container: Container) -> list:
-        pass
+    def _sorted_items(self, items: list) -> list:
+        return sorted(items, key=lambda k: getattr(k, self.sort_key), reverse=self.sort_reverse)
+
+    def _determine_sorting(self, key: str):
+        if key[0] == "-":
+            self.sort_key = key[1:]
+            self.sort_reverse = True
+        else:
+            self.sort_key = key
+            self.sort_reverse = False
+
+        if self.sort_key not in self.VALID_SORT_KEYS:
+            raise GeneratorError(f"Sort key {self.sort_key} must be one of {self.VALID_SORT_KEYS}")
+
+    def _generate_csv_output(self):
+        pd.DataFrame([c.final_dict for c in self.containers]).to_csv(self.CSV_OUTPUT_PATH, index=False)
--- container.py
+++ container.py
@@ -1,8 +1,55 @@
-from datetime import datetime, timedelta
+from copy import deepcopy
+from dataclasses import dataclass, fields
+from datetime import date, datetime, timedelta
+from typing import Union
 
 
 class ConsolidationError(Exception):
     pass
+
+
+@dataclass
+class Item:
+    pickup_location: str
+    deliver_to_location: str
+    supplier: str
+    total_volume: float
+    total_weight: float
+    ready_date: Union[date, datetime, str]
+
+    def __post_init__(self):
+        """Data validation"""
+        for f in fields(self):
+            # handle Nan/None string values
+            if f.type is not str:
+                continue
+
+            _value = getattr(self, f.name) or ""
+            if isinstance(_value, float):
+                _value = ""
+
+            if not _value:
+                setattr(self, f.name, _value)
+
+        validation_errors = []
+        if not self.pickup_location:
+            validation_errors.append(f"Pickup location: {self.pickup_location} cannot be empty")
+
+        if not self.deliver_to_location:
+            validation_errors.append(f"Deliver to location: {self.deliver_to_location} cannot be empty")
+
+        if not self.ready_date:
+            validation_errors.append(f"Ready date: {self.ready_date} cannot be empty")
+
+        if validation_errors:
+            raise TypeError(f"Item validation_errors {validation_errors}")
+
+        self.total_volume = self.total_volume or 0.0
+        self.total_weight = self.total_weight or 0.0
+
+    @property
+    def key(self) -> tuple:
+        return self.pickup_location, self.deliver_to_location
 
 
 class Container:
@@ -10,17 +57,17 @@ class Container:
     MAX_WEIGHT: float = 1500.0
     READY_DATE_WINDOW: int = 7  # days
 
-    def __init__(self, pick_up_location: str, deliver_to_location: str, item: dict):
+    def __init__(self, pick_up_location: str, deliver_to_location: str, item: Item):
         self.pick_up_location = pick_up_location
         self.deliver_to_location = deliver_to_location
         self.item = item
 
-        self.total_volume = self.item["total_volume"]
-        self.total_weight = self.item["total_weight"]
-        self.supplier = self.item["supplier"]
-        self.ready_date = item["ready_date"]
+        self.total_volume = self.item.total_volume
+        self.total_weight = self.item.total_weight
+        self.supplier = self.item.supplier
+        self.ready_date = item.ready_date
 
-        self.container_items = [self.item]
+        self.container_items = [self.item.__dict__]
 
     def __iadd__(self, other):
         if (
@@ -33,6 +80,12 @@ class Container:
         self.total_volume += other.total_volume
         self.total_weight += other.total_weight
         self.container_items.extend(other.container_items)
+
+    @property
+    def final_dict(self) -> dict:
+        _data = deepcopy(self.__dict__)
+        _data.pop("item")
+        return _data
 
     def _fails_volume(self, other_volume: float) -> bool:
         if not self.MAX_VOLUME:
